37fe0950649820fc41032dfa56be7283e5ed907f
Grazy222 <graciele222@gmail.com>
2023-02-03 20:17:44 -0300

    entregando desafio

---
+++ script.js
@@ -0,0 +1,61 @@
+//Crie uma array 5 itens
+let items = [`notebook`,`telefone`,`caderno`,`caneta`,`café`,`ferro`]
+
+//adicione mais 1 nomes no inicio
+
+items.unshift("vassoura")
+console.log(`vassoura`)
+
+//remover ultimo nome
+
+items.pop()
+console.log(items)
+
+//adicionar mais 2 nomes no final
+items.push("feijão","arroz")
+console.log(items)
+
+//remover primeiro nome
+items.shift()
+console.log(items)
+
+//organizar e ordem crescente
+//let numbers =[7,5,6,8,9,2,1,4]
+
+
+//crie um objeto que receba 3 proriedades sobre você
+let propriedades ={
+    estudo:"javaScript",
+    aprender:"dificuldade",
+    criação:"site"
+}
+
+//adicionar uma propriedade sem alterar o que foi feito
+propriedades.php = "próximo curso"
+
+//remova uma propriedade
+delete propriedades.aprender
+
+//mostre no console
+console.log(propriedades)
+
+// criar informação "cadastro" 5 objetos como:
+//nome,idade,telefone,amigos
+//amigos adicione 4 itens
+let cadastro ={
+    nome:"Grazy",
+    idade:39,
+    telefone:21991621477,
+    amigos:[`Larissa`,`Luana`,`Dayana`,`Adriana`]
+
+}
+
+console.log(cadastro)
+
+//mostre no console o nome dos amigos
+
+console.log(amigos)
+
+
+
+
